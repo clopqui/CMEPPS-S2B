1abff84e6d76c3f57b994b7028eda4fa639f6818
clopq <clopq@DESKTOP-3DIA8P2>
2024-10-04 10:55:35 +0200

    Primer Commit con la clase empleado.

---
+++ src/sesion2B/Empleado.java
@@ -0,0 +1,40 @@
+package sesion2B;
+
+public class Empleado {
+    public enum TipoEmpleado{Vendedor, Encargado}
+    
+    public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtras){
+        float salariobase=0, primas=0, extras=0;
+        
+        //determinacion salario base
+        if(tipo==TipoEmpleado.Encargado){
+            salariobase=2500;
+        } else if(tipo==TipoEmpleado.Vendedor){
+            salariobase=2000;
+        }
+        
+        //determinacion prima
+        if(ventasMes>=1500){
+            primas = 200;
+        } else if (ventasMes>=1000){
+            primas = 100;
+        }
+        
+        //determinacion extras
+        extras = horasExtras*30;
+        
+        return salariobase + primas + extras;
+    }
+    
+    public float calculoNominaNeta(float nominaBruta){
+        float retencion = 0;
+        
+        if(nominaBruta>2500){
+            retencion=0.18f;
+        } else if(nominaBruta>2000){
+            retencion=0.15f;
+        }
+        
+        return nominaBruta*(1-retencion);
+    }
+}
